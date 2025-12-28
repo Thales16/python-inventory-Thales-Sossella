2f605828fedad071d07b6e46ffc39f35b2ce858c
Thales16 <thales.sossella@gmail.com>
2025-12-27 21:14:34 -0300

    Initial setup with variables and dictionary

---
+++ Thales_coursework1.py
@@ -0,0 +1,14 @@
+inventory = {}
+
+sample_name = "Laptop" 
+sample_qty = 5         
+sample_price = 799.99  
+
+inventory["Laptop"] = {
+    "name": sample_name,
+    "quantity": sample_qty,
+    "price": sample_price
+}
+
+print("System started with one sample item.")
+print(inventory)
