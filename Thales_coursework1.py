d4e9bcc5291b0ec7c12a2adcb23bcdb09f5b30b8
Thales16 <thales.sossella@gmail.com>
2025-12-27 23:47:17 -0300

    Implemented functions to add and view items

--- Thales_coursework1.py
+++ Thales_coursework1.py
@@ -1,17 +1,28 @@
 inventory = {}
-
 categories = ["Electronics", "Home", "Office"]
-
 product_ids = set()
-product_ids.add(101) 
 
-inventory[101] = {
-    "name": "Laptop",
-    "category": "Electronics",
-    "brand": ("Dell", "USA"), 
-    "quantity": 5,
-    "price": 799.99
-}
+while True:
+    print("\nWelcome to the Inventory Management System!")
+    print("===========================================")
+    print("1. Add Item")
+    print("2. View Inventory")
+    print("3. Update Item")
+    print("4. Remove Item")
+    print("5. Exit")
+    
+    option = input("Select an option: > ")
 
-print("Data structures initialized.")
-print("Categories available:", categories)
+    if option == "1":
+        print("Add item selected (-)")
+    elif option == "2":
+        print("View inventory selected (-)")
+    elif option == "3":
+        print("Update item selected (-)")
+    elif option == "4":
+        print("Remove item selected (-)")
+    elif option == "5":
+        print("Exiting system. Goodbye!")
+        break
+    else:
+        print("Invalid option, try again.")
